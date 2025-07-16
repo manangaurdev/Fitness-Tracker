5042fc42a980fdc88b5fb201170e50f4ecb01839
Manan Gaur <manangaurdev27@gmail.com>
2025-07-16 20:21:02 +0200

    Task-0:-(Importing necessary packages)

---
+++ workspace/Project.py
@@ -0,0 +1,5 @@
+# Task-0:- Setup (We're going to use pandas for data analysis and plotly.express for interactive data visualization)
+
+# Importing necessary packages
+import pandas as pd
+import plotly.express as px 
