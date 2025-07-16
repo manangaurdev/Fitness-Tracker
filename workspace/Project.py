f379265f8fcc8ced2778922a052265c1487319fd
Manan Gaur <manangaurdev27@gmail.com>
2025-07-16 20:43:23 +0200

    Task-1:- Observed the given fitness.csv file and displayed it

--- workspace/Project.py
+++ workspace/Project.py
@@ -3,3 +3,12 @@
 # Importing necessary packages
 import pandas as pd
 import plotly.express as px 
+
+
+# Task-1:- Reading in the data (Let's read in and display the fitness_data.csv file using pandas.)
+
+# Reading in the dataset "fitness.csv"
+fitness_data = pd.read_csv("fitness_data.csv")
+
+# Display the data
+fitness_data
